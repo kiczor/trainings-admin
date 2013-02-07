baf2253453f15efa49ae273e7898fe33d6939f7c
kiczor <kiczor@gmail.com>
2013-02-07 23:23:42 +0100

    proper viewport to app.js added navigation menu to app.js added footer to app.js added header to app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,7 +8,69 @@ Ext.define('TA.app.Application', {
     launch: function() {
         Ext.create('Ext.Viewport', {
             layout : 'fit',
-            html: 'This is awsome app :)'
+            items: [{
+                xtype: 'panel',
+                layout:'auto',
+                itemId: 'mainview',
+                padding: 10,
+                minHeight: 300,
+                minWidth: 600,
+
+                dockedItems: [{
+                    dock: 'top',
+                    xtype: 'label',
+                    style: 'font-size:20px;',
+                    text: 'Trainings admin application'
+                },{
+                    dock: 'top',
+                    xtype: 'toolbar',
+                    margin: '4px 0 0 0',
+                    itemId: 'navigation',
+                    defaults: {
+                        enableToggle: true,
+                        toggleGroup: 'navigation',
+                        allowDepress: false
+                    },
+                    items: [
+                        {
+                            xtype: 'button',
+                            itemId: 'coaches',
+                            text: 'Coaches'
+                        },
+                        {
+                            xtype: 'button',
+                            itemId: 'trainings',
+                            text: 'Trainings'
+                        },
+                        {
+                            xtype: 'button',
+                            itemId: 'participants',
+                            text: 'Participants'
+                        },
+                        {
+                            xtype: 'button',
+                            itemId: 'rooms',
+                            text: 'Rooms'
+                        },
+                        {
+                            xtype: 'button',
+                            itemId: 'sessions',
+                            text: 'Sessions'
+                        },'->',
+                        {
+                            xtype: 'button',
+                            itemId: 'callendar',
+                            text: 'Callendar'
+                        }
+                    ]
+                },{
+                    dock: 'bottom',
+                    style: 'text-align: center;',
+                    margin: 5,
+                    xtype: 'component',
+                    html: 'Trainings admin v 0.1 by Training Hammer Ltd © 2012 Training Hammer ALL RIGHTS RESERVED'
+                }]
+            }]
         });
     }
 });
