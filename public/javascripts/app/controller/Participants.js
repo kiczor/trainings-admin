d352ed77f776835d2127474b40f9bc4d0d9ce0d5
kiczor <kiczor@gmail.com>
2013-02-10 16:36:52 +0100

    added add participant logic to participants controller added edit participant logic to participants controller added delete participant logic to participants contrtoller

--- public/javascripts/app/controller/Participants.js
+++ public/javascripts/app/controller/Participants.js
@@ -9,15 +9,110 @@ Ext.define('TA.controller.Participants', {
         'Participants'
     ],
 
+    views: [
+        'participant.List',
+        'participant.Add',
+        'participant.Edit'
+    ],
+
+    refs: [
+        {
+            ref: 'list',
+            selector: '',
+            xtype: 'participantlist',
+            autoCreate: true
+        },
+        {
+            ref: 'add',
+            selector: '',
+            xtype: 'participantadd',
+            autoCreate: true
+        },
+        {
+            ref: 'edit',
+            selector: '',
+            xtype: 'participantedit',
+            autoCreate: true
+        }
+    ],
+
     init: function() {
         this.callParent();
     },
 
+    onLaunch: function() {
+        this.callParent();
+        this.getList().on('addparticipantclick', this.consumeListAddParticipantClick, this);
+        this.getList().on('editparticipantclick', this.consumeListEditParticipantClick, this);
+        this.getList().on('deleteparticipantclick', this.consumeListDeleteParticipantClick, this);
+        this.getList().reconfigure(this.getStore('Participants'));
+    },
+
     destroy: function() {
+        this.getList().on('deleteparticipantclick', this.consumeListDeleteParticipantClick, this);
+        this.getList().on('editparticipantclick', this.consumeListEditParticipantClick, this);
+        this.getList().on('addparticipantclick', this.consumeListAddParticipantClick, this);
         this.callParent();
     },
 
     execute: function(params) {
-        return null;
+        this.getStore('Participants').load();
+        return this.getList();
+    },
+
+    consumeListAddParticipantClick: function(list) {
+        var addView = this.getAdd({
+            record: this.getModel('Participant').create()
+        });
+
+        addView.on('saveclick', this.consumeFormSaveClick, this);
+    },
+
+    consumeFormSaveClick: function(view) {
+        var form = view.form;
+
+        var record = form.getRecord();
+        var values = form.getValues();
+
+        record.set(values);
+        record.save({
+            success: function(record, operation) {
+                this.getStore('Participants').load();
+                view.close();
+            },
+            failure: function(record, operation) {
+                Ext.MessageBox.show({
+                    title: 'Saving participant data',
+                    msg: 'There has been an error processing your request!!!',
+                    buttons: Ext.MessageBox.OK,
+                    icon: Ext.Msg.ERROR
+                });
+            },
+            scope: this
+        });
+    },
+
+    consumeListEditParticipantClick: function(list, record) {
+        var editView = this.getEdit({
+            record: record
+        });
+        editView.on('saveclick', this.consumeFormSaveClick, this);
+    },
+
+    consumeListDeleteParticipantClick: function(list, record) {
+        record.destroy({
+            success: function() {
+                this.getStore('Participants').reload();
+            },
+            failure: function() {
+                Ext.MessageBox.show({
+                    title: 'Deleting participant',
+                    msg: 'There has been an error processing your request!!!',
+                    buttons: Ext.MessageBox.OK,
+                    icon: Ext.Msg.ERROR
+                });
+            },
+            scope: this
+        });
     }
 });
