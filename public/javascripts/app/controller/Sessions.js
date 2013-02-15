93e22173011a119b88af9e2b356da42563da566e
kiczor <kiczor@gmail.com>
2013-02-15 23:13:04 +0100

    added cell editing to rooms column of Session list

--- public/javascripts/app/controller/Sessions.js
+++ public/javascripts/app/controller/Sessions.js
@@ -42,8 +42,14 @@ Ext.define('TA.controller.Sessions', {
 
     onLaunch: function() {
         this.callParent();
+        var roomsStore = Ext.create('TA.store.Rooms');
+        roomsStore.load();
+        this.getList({
+            roomsStore: roomsStore
+        })
         this.getList().on('addsessionclick', this.consumeListAddSessionClick, this);
         this.getList().on('editsessionclick', this.consumeListEditSessionClick, this);
+        this.getList().on('sessionedited', this.consumeListSessionEdited, this);
         this.getList().on('deletesessionclick', this.consumeListDeleteSessionClick, this);
         this.getList().reconfigure(this.getStore('Sessions'));
     },
@@ -51,6 +57,7 @@ Ext.define('TA.controller.Sessions', {
     destroy: function() {
         this.getList().un('deletesessionclick', this.consumeListDeleteSessionClick, this);
         this.getList().un('editsessionclick', this.consumeListEditSessionClick, this);
+        this.getList().un('sessionedited', this.consumeListSessionEdited, this);
         this.getList().un('addsessionclick', this.consumeListAddSessionClick, this);
         this.callParent();
     },
@@ -136,6 +143,23 @@ Ext.define('TA.controller.Sessions', {
             failure: function() {
                 Ext.MessageBox.show({
                     title: 'Deleting session',
+                    msg: 'There has been an error processing your request!!!',
+                    buttons: Ext.MessageBox.OK,
+                    icon: Ext.Msg.ERROR
+                });
+            },
+            scope: this
+        });
+    },
+
+    consumeListSessionEdited: function(view, record) {
+        record.save({
+            success: function() {
+                this.getStore('Sessions').reload();
+            },
+            failure: function() {
+                Ext.MessageBox.show({
+                    title: 'Saving session data',
                     msg: 'There has been an error processing your request!!!',
                     buttons: Ext.MessageBox.OK,
                     icon: Ext.Msg.ERROR
--- public/javascripts/app/view/session/List.js
+++ public/javascripts/app/view/session/List.js
@@ -4,8 +4,21 @@ Ext.define('TA.view.session.List', {
 
     title: 'All sessions',
 
+    roomsStore: null,
+
+    plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 1,
+            listeners: {
+                'edit': function(editor, e) {
+                    e.grid.onSessionEdited(e.record);
+                }
+            }
+        })
+    ],
+
     initComponent: function() {
-        this.addEvents('addsessionclick', 'editsessionclick', 'deletesessionclick');
+        this.addEvents('addsessionclick', 'editsessionclick', 'deletesessionclick', 'sessionedited');
 
         this.dockedItems = [{
             xtype:'toolbar',
@@ -26,9 +39,20 @@ Ext.define('TA.view.session.List', {
             },
             {header: 'Starts at', xtype: 'datecolumn', format: 'Y-m-d (l)', dataIndex: 'startDate', flex: 4},
             {header: 'Ends at',  xtype: 'datecolumn', format: 'Y-m-d (l)', dataIndex: 'stopDate', flex: 4},
-            {header: 'Room', dataIndex: 'trainingRoomId', flex: 4, renderer: function(value, metaData, record, rowIndex, colIndex, store, view) {
-                return record.getRoom().get('name');
-            }
+            {header: 'Room', dataIndex: 'trainingRoomId', flex: 4,
+                renderer: function(value, metaData, record, rowIndex, colIndex, store, view) {
+                    return record.getRoom().get('name');
+                },
+                editor: {
+                    xtype: 'combobox',
+                    store: this.roomsStore,
+
+                    queryMode: 'local',
+                    displayField: 'name',
+                    valueField: 'id',
+
+                    allowBlank: false
+                }
             },
             {header: 'Coaches #', flex: 1, renderer: function(value, metaData, record, rowIndex, colIndex, store, view) {
                 return record.getCoaches().count();
@@ -72,5 +96,9 @@ Ext.define('TA.view.session.List', {
 
     onSessionDelete: function(record) {
         this.fireEvent('deletesessionclick', this, record);
+    },
+
+    onSessionEdited: function(record) {
+        this.fireEvent('sessionedited', this, record);
     }
 });
